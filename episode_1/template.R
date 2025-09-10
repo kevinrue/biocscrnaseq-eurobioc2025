77129f14c9706101e1cf626625889679f9eb954f
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2025-09-10 10:42:45 +0100

    extend the template

--- episode_1/template.R
+++ episode_1/template.R
@@ -2,28 +2,71 @@
 # Bioconductor #
 ################
 
-## Install the CRAN package 'ggplot2'
-# Hint: Use install.packages()
+## Use install.packages() to install the package 'ggplot2' from CRAN
 
 
 
-## Install the CRAN package 'BiocManager'
+## Use install.packages() to install the package 'BiocManager' from CRAN
 
 
 
-## Install the Bioconductor package 'SingleCellExperiment'
-# Hint: Use BiocManager::install()
+## Use BiocManager::install() to install the package 'SingleCellExperiment' from Bioconductor
 
 
 
-## Copy paste the instructions from the Bioconductor website to install the package 'scater'
-# Hint: navigate to https://bioconductor.org/packages/release/bioc/html/scater.html
+## Navigate to <https://bioconductor.org/packages/release/bioc/html/scater.html>
+## Then, copy paste instructions from web page to install the package 'scater'
 
 
 
-##
+## Use BiocManager::install() to update your packages to the latest version available
+
 
 
 #################################
 # The SingleCellExperiment class #
 #################################
+
+## Load the libraries 'SingleCellExperiment' and 'MouseGastrulationData'
+
+
+
+## Use 'WTChimeraData()' to load the fifth sample.
+## Assign the output to a new object called 'sce'
+
+
+
+## CHALLENGE ##
+## Get the data for a different sample from WTChimeraData (other than the fifth one).
+
+
+
+## Display the names of the assays in the 'sce' object.
+
+
+
+## Display the values in the first three rows and first three columns
+## of the counts matrix of the 'sce' object.
+
+
+
+## Display the first three rows and first four columns of the column-wise metadata
+## of the 'sce' object.
+
+
+
+## Display the first three rows and first two columns of the row-wise metadata
+## of the 'sce' object.
+
+
+
+## Use the '$' operator to add a new column named 'my_sum' to the column-wise metadata
+## of the 'sce' object.
+## The values in that column should be the sum for each column of the counts matrix.
+
+
+
+## CHALLENGE ##
+## Add a column of gene-wise metadata to the rowData.
+
+
