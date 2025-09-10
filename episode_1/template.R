49719a88721467010a9fb0210005fce8950b1b01
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2025-09-10 10:18:11 +0100

    add first bit of template for episode 1

---
+++ episode_1/template.R
@@ -0,0 +1,29 @@
+################
+# Bioconductor #
+################
+
+## Install the CRAN package 'ggplot2'
+# Hint: Use install.packages()
+
+
+
+## Install the CRAN package 'BiocManager'
+
+
+
+## Install the Bioconductor package 'SingleCellExperiment'
+# Hint: Use BiocManager::install()
+
+
+
+## Copy paste the instructions from the Bioconductor website to install the package 'scater'
+# Hint: navigate to https://bioconductor.org/packages/release/bioc/html/scater.html
+
+
+
+##
+
+
+#################################
+# The SingleCellExperiment class #
+#################################
