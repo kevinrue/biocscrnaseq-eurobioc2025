ff1e029cd289146c0f43157e0c061fd80e40453d
mireia-bioinfo <mireiarr9@gmail.com>
2025-09-16 12:36:41 +0200

    Minor format update

--- episode_6/template.R
+++ episode_6/template.R
@@ -1,11 +1,12 @@
-################
-# HCA Metadata #
-################
+###################.
+# HCA Metadata ####
+###################.
 
 # Collect metadata from sample database url
 
 
-# Get a view of the first 10 columns in the metadata with glimpse()
+# Get a view of the first 10 columns in the metadata 
+# with glimpse()
 
 
 # (Using the pipe operator)
@@ -16,18 +17,19 @@
 
 # Do the same for the assay types:
 
-#...........#
-# Challenge #
-#...........#
-# Look through the full list of metadata column names. Do any other metadata 
-# columns jump out as interesting to you for your work?
+#...............#
+# Challenge #####
+#...............#
+# Look through the full list of metadata column names. 
+# Do any other metadata columns jump out as interesting 
+# to you for your work?
 
 
 
 
-################################
-# Downloading single cell data #
-################################
+###################################.
+# Downloading single cell data ####
+###################################.
 
 # Identify cells meeting the following criteria:
 #     - African ethnicity
@@ -52,38 +54,42 @@
 # Save your SingleCellExperiment 
 
 
-#######################################
-# Exercise 1: Basic counting + piping #
-#######################################
-# Use count and arrange to get the number of cells per tissue in descending 
-# order.
+#########################################.
+# Exercise 1: Basic counting + piping ####
+#########################################.
+# Use count and arrange to get the number of cells per tissue 
+# in descending order.
 
 
-######################################
-# Exercise 2: Tissue & type counting #
-######################################
-# count() can group by multiple factors by simply adding another grouping column 
-# as an additional argument. Get a tally of the highest number of cell types per 
-# tissue combination. What tissue has the most numerous type of cells?
-
-
-
-#############################################
-# Exercise 3: Comparing metadata categories #
-#############################################
-# Spot some differences between the tissue and tissue_harmonised columns. Use 
-# count to summarise.
+#########################################.
+# Exercise 2: Tissue & type counting ####
+#########################################.
+# count() can group by multiple factors by simply adding another 
+# grouping column as an additional argument. Get a tally of the 
+# highest number of cell types per tissue combination. What tissue 
+# has the most numerous type of cells?
 
 
 
-###########################################
-# Exercise 4: Highly specific cell groups #
-###########################################
-# Now that we are a little familiar with navigating the metadata, let’s obtain a
-# SingleCellExperiment of 10X scRNA-seq counts of cd8 tem lung cells for females
-# older than 80 with COVID-19. Note: Use the harmonized columns, where possible.
+################################################.
+# Exercise 3: Comparing metadata categories ####
+################################################.
+# Spot some differences between the tissue and tissue_harmonised 
+# columns. Use count to summarise.
 
 
 
+##############################################.
+# Exercise 4: Highly specific cell groups ####
+##############################################.
+# Now that we are a little familiar with navigating the metadata, 
+# let’s obtain a SingleCellExperiment of 10X scRNA-seq counts of 
+# cd8 tem lung cells for females older than 80 with COVID-19. Note: 
+# Use the harmonized columns, where possible.
 
+
+
+###########.
+# THE END #
+###########.
 
